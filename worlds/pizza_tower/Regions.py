f6e02a615a08a54e15533f3c6728cd579e50fedf
unsafetyskizzers <195637029+unsafetyskizzers@users.noreply.github.com>
2026-06-10 20:52:42 -0500

    finished level room regions list

--- worlds/pizza_tower/Regions.py
+++ worlds/pizza_tower/Regions.py
@@ -22,6 +22,9 @@ def create_regions(world: World, multiworld: MultiWorld, player: int):
         #       "[level] Gerome"
         #       "[level] Key"
         #       "[level] Lap 2 Portal"
+        #       ...and others for chef tasks, as needed
+        # also don't forget to place "lap 2" event items at wherever lap 2 starts (usually john), not in the lap portal room itself
+        # also also don't forget pumpkins cuz they don't show up on the sr wiki
 
         # hub world floors
         # TODO make tower_1 the starting region in init.py
@@ -45,13 +48,13 @@ def create_regions(world: World, multiworld: MultiWorld, player: int):
         "entrance_9", # pineapple + secret 2 + gerome
         "entrance_10", # john
         "entrance_6c", # escape + secret 3
-        "entrance_lap", # lap portal
+        "entrance_lap",
 
         # for spoiler log clarity, name secrets as normal, not by their internal name
         # this will be done through a loop
 
         # pizzascape
-        "medieval_1", # starting room and lap portal and mushroom
+        "medieval_1", # starting room + mushroom
         "medieval_2", # the insta isn't THAT hard, guys
         "medieval_3", # first knight room + cheese
         "medieval_3b", # secret 3
@@ -62,11 +65,11 @@ def create_regions(world: World, multiworld: MultiWorld, player: int):
         "medieval_8", # key
         "medieval_9", # weird ass updoor room
         "medieval_9b", # pineapple + gerome
-        "medieval_10", # john
+        "medieval_10", # john + last priest required for shining armor
         # "medieval" doesnt even look like a real word anymore
 
         # ancient cheese
-        "ruin_1", # start + locked door + lap portal
+        "ruin_1", # start + locked door
         "ruin_2", # mushroom + secret 1 + delicacy
         "ruin_3", # fork room
         "ruin_3b", # gerome
@@ -83,7 +86,7 @@ def create_regions(world: World, multiworld: MultiWorld, player: int):
         "ruin_13", # escape + superjump room
 
         # bloodsauce dungeon
-        "dungeon_1", # start + lap portal
+        "dungeon_1", # start
         "dungeon_2", # mushroom
         "dungeon_3", # secret 1 + cheese
         "dungeon_4", # tomato + gerome
@@ -93,4 +96,272 @@ def create_regions(world: World, multiworld: MultiWorld, player: int):
         "dungeon_8", # second big bodyslam
         "dungeon_9", # sausage + pineapple + secret 3
         "dungeon_10", # john
+
+        # oregano desert
+        "badland_1", # start
+        "badland_2", # cool highjump room
+        "badland_3", # entrance to mart 1 + return from escape
+        "badland_mart1", # mushroom
+        "badland_4", # secret 1
+        "badland_5 1F", # this room is split into 3 parts. this part is the bottom floor
+        "badland_5a", # first firemouth room + entrance to mart 2
+        "badland_mart2", # cheese
+        "badland_5 2F", # middle floor of badland_5
+        "badland_5b", # hot wing skip room + gerome
+        "badland_5 3F", # top floor of badland_5
+        "badland_8", # secret 2
+        "badland_8b", # entrance to ufo
+        "badland_9", # ufo + entrance to mart 3 + john
+        "badland_mart4", # actually mart 3. why are the marts numbered like this. fuck you mcpig. + tomato
+        "badland_10", # may not actually be called this but it's not on the sr wiki so idk + treasure + mart 4 entrance
+        "badland_mart5", # actually mart 4
+        "badland_6", # funny cow escape room
+        "badland_7", # entrance to mart 5
+        "badland_mart3", # actually mart 5 + secret 3 + pineapple
+
+        # wasteyard
+        "graveyard_1", # start
+        "graveyard_2", # mushroom + escape leads to graveyard_7
+        "graveyard_3", # secret 1
+        "graveyard_4", # first ghost room + cheese + secret 3 (escape-only)
+        "graveyard_5", # tomato + gerome
+        "graveyard_5b", # second ghost room
+        "graveyard_5c", # secret 2 + sausage + escape leads to graveyard_3
+        "graveyard_6", # treasure + john
+        "graveyard_7", # funny hywoost room
+        "graveyard_8", # pineapple
+        "graveyard_9",
+        "graveyard_9b", # last ghost room
+        "graveyard_10", # long corpse room
+
+        # fun farm
+        "farm_2", # start. yes, farm_2 really is the start room. don't ask
+        "farm_1", # treasure
+        "farm_3",
+        "farm_4", # escape reconnects to main path
+        "farm_4b", # secret 1
+        "farm_5", # highjump room
+        "farm_7", # first mort room
+        "farm_8", # mushroom (finally)
+        "farm_cube", # mort cube
+        "farm_6", # gerome + cheese
+        "farm_9", # second mort room
+        "farm_10", # tomato
+        "farm_9b", # sausage + secret 2
+        "farm_11", # john
+        "farm_12", # no one is safe
+        "farm_13", # pineapple + secret 3
+
+        # fastfood saloon
+        "saloon_1", # did you guys know that there are hidden points if you climb the left wall? + start
+        "saloon_lap",
+        "saloon_2", # mushroom
+        "saloon_2b", # secret 3 (escape-only)
+        "saloon_3b", # first weenie room
+        "saloon_3", # treasure + cheese
+        "saloon_4", # tomato
+        "saloon_4b", # another weenie room
+        "saloon_5b", # secret 1
+        "saloon_5", # sausage + secret 2 + key
+        "saloon_6b", # pineapple + locked door
+        "saloon_6", # gerome + john... waow <3
+
+        # crust cove
+        "plage_entrance", # start
+        "plage_lap",
+        "plage_beach1", # mushroom + treasure
+        "plage_shipmain", # locked door that leads to beach2 + escape route to shiptop
+        "plage_ship1", # first barrel room
+        "plage_ship2", # cheese + secret 1
+        "plage_ship3", # key + back to shipmain
+        "plage_beach2", # tomato + secret 2
+        "plage_cavern1", # there's a treasure guy here but that's kinda it
+        "plage_cavern2", # sausage + secret 3 + john
+        "plage_cavern3", # leads back to beach2
+        "plage_shiptop", # pineapple
+
+        # gnome forest
+        "forest_1", # start + secret 3 (escape-only)
+        "forest_2", # bothersome lumberjack location
+        "forest_lap",
+        "forest_3", # treasure + pineapple (escape-only)
+        "forest_G1", # switch to gustavo
+        "forest_G1b", # gustavo tutorial. the latter part of G1 is considered to be part of G1b just to make things easier on me
+        "forest_G2", # secret 1
+        "forest_G3", # escape reconnects to main path
+        "forest_5",
+        "forest_6", # mushroom
+        "forest_7",
+        "forest_G4", 
+        "forest_G5", # cheese
+        "forest_8",
+        "forest_9",
+        "forest_10", # tomato
+        "forest_12", # secret 2
+        "forest_14",
+        "forest_15", # meme room
+        "forest_16", # gerome
+        "forest_17", # sausage
+        "forest_john", # john
+        "forest_escape2", # no, i don't know why the escape rooms are switched like this
+        "forest_escape1", # leads back to forest_G3
+
+        # deez dish 9
+        "space_1", # start + treasure + first rocket room + first bubble room
+        "space_lap",
+        "space_2",
+        "space_3", # mushroom + secret 1
+        "space_4", # cheese + leads back to space_3
+        "space_5",
+        "space_6", # tomato
+        "space_7", # gerome + secret 2 + man meteor
+        "space_8", # sausage
+        "space_9", # pineapple + john
+        "space_10", # turbo tunnel
+        "space_11",
+        "space_11b", # secret 3
+        "space_12",
+
+        # golf
+        "minigolf_1", # start
+        "minigolf_lap",
+        "minigolf_2",
+        "minigolf_3",
+        "minigolf_4", # mushroom + hub area for all following pre-escape rooms
+        "minigolf_5", # cheese + secret 1
+        "minigolf_6", # tomato + secret 2
+        "minigolf_7", # sausage + secret 3
+        "minigolf_8", # gerome + pineapple + john. escape can go straight to minigolf_4 in expert logic
+        "minigolf_9",
+        "minigolf_10",
+        "minigolf_11", # back to minigolf_8 but we can skip to minigolf_4 if we need to
+
+        # pig city
+        "street_intro", # start
+        "street_1",
+        "street_house1", # mushroom
+        "street_2",
+        "street_house2", # cheese + secret 1
+        "street_3",
+        "street_house3", # tomato
+        "Bacon Room", # idk the internal name for this one but it has a regular name anyway
+        "street_jail", # treasure + switch to gustavo
+        "street_4",
+        "street_house4", # sausage + secret 2
+        "street_5", # strike! + last pig needed for say oink!
+        "street_house5", # pineapple + gerome + secret 3
+        "street_john", # john
+
+        # peppibot factory - most of these rooms are split in halves so they'll have weird names
+        "industrial_1", # start
+        "industrial_lap",
+        "Left side of industrial_2", # treasure (escape-only) + mushroom + secret 1
+        "Left side of industrial_3", # cheese
+        "Left side of industrial_4", # gerome + secret 2 + tomato
+        "industrial_5", # sausage + john
+        "Right side of industrial_4", # pineapple
+        "Right side of industrial_3", # secret 3 + last priest required for unflattening
+        "Right side of industrial_2", # last peppibot required for there can only be one (lap 2 only)
+
+        # oh shit!
+        "sewer_1", # start. sticky cheese is not actually needed to pass this room
+        "sewer_2", # if pieces of a room are connected by pipes, just count them as one room
+        "sewer_3", # mushroom + secret 1
+        "sewer_4", # cheese
+        "sewer_5", # first sticky cheese room (technically)
+        "sewer_6", # gerome
+        "sewer_7", # tomato + earliest place to get food clan
+        "sewer_8", # secret 2 + john
+        "sewer_9", # sausage
+        "sewer_10", # treasure
+        "sewer_11", # pineapple + secret 3
+        "sewer_12", # back to sewer_2
+
+        # rrf - noise's route looks way different so be careful
+        # peppino is expected to lose satan's choice at lap 2 so we need different rules to account for that
+        # ideas:
+        #   - make a separate region route for lap 2
+        #   - add a stupidly long list of rules (like we did before) to freezerator p rank
+        #   - maybe some other archipelago feature i haven't heard of yet?
+        "freezer_1", # start + mushroom
+        "freezer_lap",
+        "freezer_2",
+        "freezer_3", # cheese + treasure
+        "freezer_4",
+        "freezer_5", # secret 3 (escape-only)
+        "freezer_6", # tomato + secret 2
+        "freezer_7", # escape leads to freezer_4
+        "freezer_9", # freezer_11 is considered part of freezer_9 to make things easier
+        "freezer_12",
+        "freezer_13", # sausage + escape reconnects to main path
+        "freezer_15", # pineapple + secret 2
+        "freezer_16", # we can ignore freezer_upgrade and just assume the player has satan's choice during escape
+        "freezer_17",
+        "freezer_escape1", # john + back to freezer_13
+
+        # pizzascare
+        "chateau_1", # start + secret 3 (escape-only)
+        "chateau_2", # pumpkin
+        "chateau_2b", # treasure (escape-only)
+        "chateau_3", # mushroom
+        "chateau_4",
+        "chateau_5", # cheese + secret 2 (escape-only) + gerome (escape-only)
+        "chateau_6", # tomato
+        "chateau_7", # ignore chateau_6 in escape
+        "chateau_8", # sausage + secret 1
+        "chateau_9", # john + i think pineapple is here? it's not on the sr wiki though
+
+        # dmas
+        "kidsparty_1", # start
+        "kidsparty_floor1_1",
+        "kidsparty_floor1_2", # mushroom + secret 1. ignore kidsparty_basementsecret
+        "kidsparty_floor1_3",
+        "kidsparty_floor2_1",
+        "kidsparty_floor2_2", # cheese
+        "kidsparty_floor2_3", # secret 2 + treasure (escape-only)
+        "kidsparty_floor3_1",
+        "kidsparty_floor3_2", # tomato
+        "kidsparty_floor3_3", # secret 3
+        "kidsparty_floor4_1",
+        "kidsparty_floor4_2", 
+        "kidsparty_floor4_3", # gerome
+        "kidsparty_john", # john
+        # TODO add unlisted escape rooms
+
+        # war - no john in this level so rules are gonna be easier to write
+        "war_1", # start
+        "war_2", # mushroom
+        "war_3", # cheese + gerome
+        "war_6", # tomato
+        "war_7", # secret 1
+        "war_8", # sausage
+        "war_9", # secret 2
+        "war_10",
+        "war_11",
+        "war_12", # secret 3
+        "war_12b", # treasure
+        "war_13", # pineapple + finish
+        "war_lap", # lap portal actually leads here
+
+        # ctop - like war, this level is linear so rules are easy
+        "tower_finalhallway", # ignore john. also assume player always starts at the gate
+        "tower_5 (Escape)", # make sure to distinguish hub rooms from ctop rooms, since they're functionally different
+        "tower_escape1", # shotgun pickup
+        "tower_escape2",
+        "tower_escape3",
+        "tower_4 (Escape)"
+        "tower_escape4",
+        "tower_escape5", # weenie room
+        "tower_escape6", # rocket room
+        "tower_3 (Escape)",
+        "tower_escape7",
+        "tower_escape8",
+        "tower_escape9",
+        "tower_2 (Escape)",
+        "tower_escape10",
+        "tower_escape11",
+        "tower_escape12",
+        "tower_1 (Escape)",
+        "tower_johngutterhall",
+        "tower_entrancehall" 
     ]
